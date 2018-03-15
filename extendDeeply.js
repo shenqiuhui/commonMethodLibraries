2cdd88172ce50d43f9286c16f0ec1a37976f8f32
PandaShen <pandashen821@163.com>
2018-03-16 00:52:22 +0800

    深复制对象函数封装

---
+++ extendDeeply.js
@@ -0,0 +1,41 @@
+/**
+ * 深复制对象
+ * @param {Object} options
+ * @param {Object} target
+ */
+function extendDeeply(options, target) {
+    var target = target || {};
+    for(var prop in options) {
+        if(typeof options[prop] == 'object') {
+            target[prop] = options[prop] instanceof Array ? [] : {};
+            arguments.callee(options[prop], target[prop]);
+        } else {
+            target[prop] = options[prop];
+        }
+    }
+    return target;
+}
+
+
+// 示例
+var obj = {
+    name: 'panda',
+    subObj: {
+        age: 20,
+        fn: function () {
+            console.log(1);
+        }
+    },
+    hobby: ['basketball', 'swim'],
+    f: function () {
+        console.log(2);
+    }
+};
+
+var newObj = extendDeeply(obj);
+console.log(newObj);
+console.log(newObj === obj);
+console.log(newObj.subObj === obj.subObj);
+console.log(newObj.hobby === obj.hobby);
+console.log(newObj.subObj.fn === obj.subObj.fn);
+console.log(newObj.f === obj.f);
